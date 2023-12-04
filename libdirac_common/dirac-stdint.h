693772fc113ea8f21dc7aaa8cc2e7e848b5d55bb
souwang324 <33944965+souwang324@users.noreply.github.com>
2023-12-04 21:33:35 +0900

    Initial commit

---
+++ libdirac_common/dirac-stdint.h
@@ -0,0 +1,9 @@
+#ifndef _DIRAC_LIBDIRAC_COMMON_DIRAC_STDINT_H
+#define _DIRAC_LIBDIRAC_COMMON_DIRAC_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "dirac 1.0.2"
+/* generated using gnu compiler gcc (SUSE Linux) 4.3.2 [gcc-4_3-branch revision 141291] */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
